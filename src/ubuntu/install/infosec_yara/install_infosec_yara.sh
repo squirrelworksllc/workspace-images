28518c2d1db5cb233154bcfaa6916797144d47ae
James Konderla <james.konderla@konderla.org>
2025-12-13 22:34:01 -0600

    Drafted undeveloped yara install script. Needs testing

--- src/ubuntu/install/infosec_yara/install_infosec_yara.sh
+++ src/ubuntu/install/infosec_yara/install_infosec_yara.sh
@@ -1,50 +1,58 @@
-#TODO 12122025 - Divest all the docker and base vscode crap and configure this to JUST install Yara and Yara capabilities
-# Also make this better, this can be included on top of the other ubuntu builds now!
-# Maybe there are other yara capabilities and extensions now? A newer Yara?
+# This script is designed to install the complete environment needed to test and/or develop YARA rules inside
+# of a docker container. This includes a pre-defined Yara release, a Yara Language Server (YLS-Yara)
+# and extensions for Visual Studio as well as commandline utilities.
+# Although this script may work alone it was not designed to do so and was designed to be invoked via Dockerfile.
+#!/usr/bin/env bash
+set -ex
 
+echo "======= Installing InfoSec Yara Environment ======="
+# Install Dependencies
+echo "Step 1: Installing Dependencies..."
+apt install automake libtool make gcc pkg-config libssl-dev -y
 
-# DEV Note - All under this line is WIP
+# Download Yara 4.5.5 release and install
+echo "Step 2: Download and Install Yara..."
+cd /opt
+curl -sSLo yara.tar.gz https://github.com/VirusTotal/yara/archive/refs/tags/v4.5.5.tar.gz
+tar -zxf yara.tar.gz
+cd /opt/yara
+./bootstrap.sh
+./configure
+echo "Now Installing Yara 4.5.5..."
+make install
 
-#############################################
-### Docker file for GTMR YARA Environment
-### Author: James Konderla
-### Version: 3
-### Created: 12/1/2023
-### Updated: 7/16/2025
-#############################################
-FROM kasmweb/ubuntu-jammy-desktop:1.17.0-rolling-weekly
-USER root
-ENV HOME=/home/kasm-default-profile
-ENV STARTUPDIR=/dockerstartup
-ENV INST_SCRIPTS=$STARTUPDIR/install
-WORKDIR $HOME
-######### Customize Container Here ###########
-RUN apt-get update && apt-get update -y
-RUN apt-get install libssl-dev automake libtool yara -y
-RUN pip3 install yara-python
-RUN pip3 install -U yls-yara
-# Begin Step: Add VSCode to repository
-RUN curl -sSL -O https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb
-RUN dpkg -i packages-microsoft-prod.deb
-# End Step
-# Begin Step: Install apps and prepare Python venv
-RUN apt update
-RUN apt install python3-venv -y
-RUN mkdir -p ~/yls && cd ~/yls
-RUN python3 -m venv env yls/env/bin/activate
-# End Step
-# Begin Step: Create Icons and Install VSCode extensions
-RUN cd /usr/share/applications && sed -i 's%--new-window% c --no-sandbox --disable-workspace-trust --new-window%' code.desktop
-RUN cd /usr/share/applications && sed -i 's%--unity-launch% c --no-sandbox --disable-workspace-trust --new-window%' code.desktop
-RUN mkdir -p /home/kasm-user/.local/share/applications && cp /usr/share/applications/code.desktop /home/kasm-user/.local/share/applications/code.desktop
-RUN code --no-sandbox --user-data-dir ~/.config/Code --install-extension infosec-intern.yara
-RUN code --no-sandbox --user-data-dir ~/.config/Code --install-extension avast-threatlabs-yara.vscode-yls
-# End Step
-######### End Customizations ###########
-RUN chown 1000:0 $HOME
-RUN $STARTUPDIR/set_user_permission.sh $HOME
-ENV HOME=/home/kasm-user
-WORKDIR $HOME
-RUN mkdir -p $HOME && chown -R 1000:0 $HOME
-USER 1000
-##################### EOF ###################
+# Download Yara's Python libraries
+echo "Step 3: Download and install Python libraries..."
+# Install yara-python
+pip3 install yara-python
+# Prep and install yls-yara
+mkdir -p ~/yls
+cd ~/yls
+python -m venv env
+. env/bin/activate
+pip3 install -U yls-yara
+# To get the absolute path of the yls executable
+realpath env/bin/yls
+
+# Install VS Code extension
+echo "Step 4: Prep and Install VS Code extension..."
+cd /usr/share/applications && sed -i 's%--new-window% c --no-sandbox --disable-workspace-trust --new-window%' code.desktop
+cd /usr/share/applications && sed -i 's%--unity-launch% c --no-sandbox --disable-workspace-trust --new-window%' code.desktop
+mkdir -p /home/kasm-user/.local/share/applications && cp /usr/share/applications/code.desktop /home/kasm-user/.local/share/applications/code.desktop
+code --no-sandbox --user-data-dir ~/.config/Code --install-extension infosec-intern.yara
+code --no-sandbox --user-data-dir ~/.config/Code --install-extension avast-threatlabs-yara.vscode-yls
+echo "VS Code should now be ready."
+
+# Cleanup for app layer
+echo "Step 5: Cleaning up..."
+chown -R 1000:0 $HOME
+find /usr/share/ -name "icon-theme.cache" -exec rm -f {} \;
+if [ -z ${SKIP_CLEAN+x} ]; then
+  apt-get autoclean
+  rm -rf \
+    /var/lib/apt/lists/* \
+    /var/tmp/* \
+    /tmp/*
+fi
+
+echo "InfoSec Yara Environment is now installed!"
